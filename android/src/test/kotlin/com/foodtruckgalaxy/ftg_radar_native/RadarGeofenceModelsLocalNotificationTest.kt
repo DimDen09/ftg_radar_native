1bbf662c33a838e369b09fa835986b9057e954b5
DimDen09 <pfsdenisd@gmail.com>
2026-08-23 21:09:30 +0200

    test(android): run JSON persistence checks with Robolectric

--- android/src/test/kotlin/com/foodtruckgalaxy/ftg_radar_native/RadarGeofenceModelsLocalNotificationTest.kt
+++ android/src/test/kotlin/com/foodtruckgalaxy/ftg_radar_native/RadarGeofenceModelsLocalNotificationTest.kt
@@ -4,7 +4,10 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class RadarGeofenceModelsLocalNotificationTest {
     @Test
     fun `truck name survives geofence persistence`() {
